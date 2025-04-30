ee6011fca5e300effd3f4e8a94e4aa8fb9686a1f
DebabrataHalder <devhalder321@gmail.com>
2025-05-01 01:47:51 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def main():
+    """
+    Entry point for Awesome Project.
+    Right now it just prints a welcome message.
+    """
+    print("🚀 Welcome to Awesome Project!")
+
+if __name__ == "__main__":
+    main()
