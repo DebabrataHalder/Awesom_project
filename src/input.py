ce6050aaebeca6eb2546af943a831efbdbe686d8
Debabrata Halder <108052492+DebabrataHalder@users.noreply.github.com>
2025-05-01 03:50:24 +0530

    Update input.py

--- src/input.py
+++ src/input.py
@@ -1,9 +1,1 @@
-def get_inputs():
-    try:
-        num1 = float(input("Enter first number: "))
-        operator = input("Enter operator (+, -, *, /): ").strip()
-        num2 = float(input("Enter second number: "))
-        return num1, operator, num2
-    except ValueError:
-        print("Invalid input. Please enter numeric values.")
-        return None, None, None
+
